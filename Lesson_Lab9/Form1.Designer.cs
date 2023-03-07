cc7a8a290b64914b6a05b9a7549d8be8f9c752d3
USER <USER@DESKTOP-TRQG01D>
2023-03-07 12:21:15 +0300

    Добавьте файлы проекта.

---
+++ Lesson_Lab9/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Lesson_Lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textKolvo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textKolvo2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textKolvo
+            // 
+            this.textKolvo.Location = new System.Drawing.Point(170, 99);
+            this.textKolvo.Name = "textKolvo";
+            this.textKolvo.Size = new System.Drawing.Size(83, 23);
+            this.textKolvo.TabIndex = 1;
+            this.textKolvo.TextChanged += new System.EventHandler(this.textKolvo_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Подсчитать количество символов \"а\"";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textKolvo2
+            // 
+            this.textKolvo2.Location = new System.Drawing.Point(278, 196);
+            this.textKolvo2.Name = "textKolvo2";
+            this.textKolvo2.Size = new System.Drawing.Size(92, 23);
+            this.textKolvo2.TabIndex = 3;
+            this.textKolvo2.TextChanged += new System.EventHandler(this.textKolvo2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(328, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Подсчитать количество вхождений сочетания букв \"ма\"";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите строку:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Количество символов \"а\":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Количество вхождений сочетания букв «ма» :";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(295, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 272);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textKolvo2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textKolvo);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Строковые функции";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textKolvo;
+        private Button button1;
+        private TextBox textKolvo2;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button3;
+    }
+}
